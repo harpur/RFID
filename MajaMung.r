525cf63bccc7fd06bb8675b4e9e3e381ae431ec7
Brock A Harpur <b.harpur@gmail.com>
2020-09-23 11:50:58 -0400

    fixed = and added file check

--- MajaMung.r
+++ MajaMung.r
@@ -28,17 +28,24 @@ require("dataframes2xls")
 
 
 #Read in Dataframe --------------------------
-args = commandArgs(trailingOnly=TRUE)
-raw.data = read.xls(args[1]) #"Sample RFID Data Set (1).xlsx"
+args <- commandArgs(trailingOnly=TRUE) #'example.xlsx'
+
+
+if(file.exists(args[1])){
+	raw.data <- read.xls(args[1]) 
+}else{
+	print('input file does not exist')
+}
+
 
 #convert columns to time, date, etc ---------------
-raw.data[ ,1] = as.POSIXct(raw.data[ ,1], format="%m/%d/%Y %I:%M:%S %p")  ## Convert to usable date and time of creation (in the PM format)
+raw.data[ ,1] <- as.POSIXct(raw.data[ ,1], format="%m/%d/%Y %I:%M:%S %p")  ## Convert to usable date and time of creation (in the PM format)
 options(digits.secs=3); raw.data[ ,4] = as.POSIXct(raw.data[ ,4], format="%m/%d/%Y %H:%M:%OS") ## convert reading time to usable in R with milliseconds (and printed milliseconds)
-raw.data$UTCTime_Round = round(raw.data$UTCTime, units = "secs") ## round the milliseconds proper
-raw.data = raw.data[order(raw.data[,4 ]),] ## order by date and time
-raw.data = raw.data[order(raw.data$UID),] ## oreder by ID
-raw.data$Date = lapply(strsplit(as.character(raw.data$UTCTime), " "), "[", 1) ## Get a column with just dates
-bees = as.character(unique(raw.data$UID))
+raw.data$UTCTime_Round <- round(raw.data$UTCTime, units = "secs") ## round the milliseconds proper
+raw.data <- raw.data[order(raw.data[,4 ]),] ## order by date and time
+raw.data <- raw.data[order(raw.data$UID),] ## oreder by ID
+raw.data$Date <- lapply(strsplit(as.character(raw.data$UTCTime), " "), "[", 1) ## Get a column with just dates
+bees <- as.character(unique(raw.data$UID))
 
 
 #Output some data checks --------------------------------
@@ -46,37 +53,37 @@ print(paste("there are ", length(bees), "samples in your raw dataset", sep=" "))
 print(bees)
 
 
-RF = raw.data ## I just didn't want to change it in the code 
+RF <- raw.data ## I just didn't want to change it in the code 
 
-mung.df = c()
+mung.df <- c()
 for(bee in bees){ ##Creating a loop for each variable in dataframe "bees"
 	#
-	test = RF[which(RF$UID==bee), ]
-	Dates = as.character(unique(test$Date)) ##creating a list of unique dates
+	test <- RF[which(RF$UID==bee), ]
+	Dates <- as.character(unique(test$Date)) ##creating a list of unique dates
 	#low number of rows will cause failure
 	for(Date in Dates){  ##creating a loop for each variable in dataframe "Dates"
 		#
-		date.df=test[which(test$Date==Date), ]
-		ad=date.df$Address;ad=c(ad, 0) ##creating directionality -- new list of all the column Address plus a zero at the end
-		ad1=date.df$Address; ad1=c(0, ad1) ##creating directionality -- new list of all the column Address plus a zero at the beginning
-		ad=paste(ad1, ad, sep="-") ##pasting the two lists together
+		date.df <- test[which(test$Date==Date), ]
+		ad <- date.df$Address;ad=c(ad, 0) ##creating directionality -- new list of all the column Address plus a zero at the end
+		ad1 <- date.df$Address; ad1=c(0, ad1) ##creating directionality -- new list of all the column Address plus a zero at the beginning
+		ad <- paste(ad1, ad, sep="-") ##pasting the two lists together
 		
-		ax=date.df$UTCTime; ax=c(ax,ax[length(ax)]) ##listing all the times plus an entery that equals the last time
-		ax1=date.df$UTCTime; ax1=c(ax1[1], ax1) ##listing all the times plus an entery that equals the fist time
-		ax=as.numeric(difftime(ax, ax1, unit="sec")) ##subtracting the two lists and getting output as numeric in seconds
+		ax <- date.df$UTCTime; ax=c(ax,ax[length(ax)]) ##listing all the times plus an entery that equals the last time
+		ax1 <- date.df$UTCTime; ax1=c(ax1[1], ax1) ##listing all the times plus an entery that equals the fist time
+		ax <- as.numeric(difftime(ax, ax1, unit="sec")) ##subtracting the two lists and getting output as numeric in seconds
 		
-		ax = ax[-1] #removing last row
-		ad=ad[-1] #removing last row
-		date.df$timediff = ax ##adding the time difference column to dataframe
-		date.df$dir = ad ##adding the directionality column to dataframe
-		mung.df = rbind(date.df,mung.df) ##merging the to dateframes without repeats
+		ax <- ax[-1] #removing last row
+		ad <- ad[-1] #removing last row
+		date.df$timediff <- ax ##adding the time difference column to dataframe
+		date.df$dir <- ad ##adding the directionality column to dataframe
+		mung.df <- rbind(date.df,mung.df) ##merging the to dateframes without repeats
 		print(Date)
 
 	}
 	print(bee)
 }
 
-mung.df = mung.df[c("UTCTime_Round", "UID", "timediff", "dir")]
+mung.df <- mung.df[c("UTCTime_Round", "UID", "timediff", "dir")]
 
 
 write.xls(mung.df, file = paste(args[2], ".xlsx",sep=""))
